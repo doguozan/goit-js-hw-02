bc44ea65bdbbb5ebf8aef251e525ef4a7470027d
dogu ozan <doguozannn3gmail.com>
2024-10-11 18:53:35 +0300

    first commit

---
+++ task-2.js
@@ -0,0 +1,13 @@
+function formatMessage(message, maxlength) {
+  if (message.length <= maxlength) {
+    return message;
+  }
+
+  return message.slice(0, maxlength) + "...";
+}
+console.log(formatMessage("Curabitur ligula sapien", 16));
+console.log(formatMessage("Curabitur ligula sapien", 23));
+console.log(formatMessage("Vestibulum facilisis purus nec", 20));
+console.log(formatMessage("Vestibulum facilisis purus nec", 30));
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15));
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41));
